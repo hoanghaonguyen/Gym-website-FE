ff008dc1d3e7eba49b7e9e24f0000cff06cbff08
hoanghao01 <haonguyen6345@gmail.com>
2024-09-03 23:00:19 +0700

    add footer

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -5,6 +5,7 @@ import Program from '../../components/Program'
 import Values from '../../components/Values'
 import FAQs from '../../components/FAQs'
 import Testimonials from '../../components/Testimonials'
+import Footer from '../../components/Footer'
 
 const Home = () => {
   return (
@@ -14,6 +15,7 @@ const Home = () => {
       <Values />
       <FAQs />
       <Testimonials />  
+      <Footer />
     </>
   )
 }
