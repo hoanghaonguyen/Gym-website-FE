d24988de1b9dd2cf2f95919afba0b749e3c630d6
hoanghaonguyen <haohao6345@gmail.com>
2024-09-12 07:34:10 +0700

    ud1

--- src/pages/contact/Contact.js
+++ src/pages/contact/Contact.js
@@ -1,9 +1,27 @@
 import React from 'react'
+import Banner from '../../components/Banner'
+import HeaderImage from '../../images/header_bg_2.jpg'
+import { MdEmail } from 'react-icons/md'
+import { BsMessenger } from 'react-icons/bs'
+import { IoLogoWhatsapp } from 'react-icons/io'
 import './ contact.css'
 
-const Contact = () => {
+const Contact = ()=> {
   return (
-    <div>Contact</div>
+    <>
+    <Banner title="Get In Touch" image={HeaderImage}>
+      Fruga  itaque ducimus, ad amet praesentium suscipit sit, assumenda nihil officia totam, animi culpa nobis nemo natus doloremque?
+    </Banner>
+    <section className='contact'>
+      <div className='container contact__container'>
+        <div className='contact__wrapper'>
+          <a href='example@gmail.com' target='_blank' rel='noreferrer noopener'><MdEmail/></a>
+          <a href='#' target='_blank' rel='noreferrer noopener'><BsMessenger/></a>
+          <a href='0123456789' target='_blank' rel='noreferrer noopener'><IoLogoWhatsapp/></a>
+        </div>  
+      </div>
+    </section> 
+    </>
   )
 }
 
--- src/pages/gallery/Gallery.js
+++ src/pages/gallery/Gallery.js
@@ -1,9 +1,32 @@
 import React from 'react'
+import Banner from '../../components/Banner'
+import HeaderImage from '../../images/header_bg_3.jpg'
 import './gallery.css'
 
 const Gallery = () => {
+  const galleryLengh = 15;
+  const images = [];
+
+  for (let i = 1; i <= galleryLengh; i++) {
+    images.push(require(`../../images/gallery${i}.jpg`))
+  }
   return (
-    <div>Gallery</div>
+    <>
+      <Banner title="Our Gallery" image={HeaderImage}>
+        Fruga  itaque ducimus, ad amet praesentium suscipit sit, assumenda nihil officia totam, animi culpa nobis nemo natus doloremque?
+      </Banner>
+      <section className='gallery'>
+        <div className='container gallery__container'>
+          {
+            images.map((image, index) => {
+              return <article key={index}>
+                <img src={image} alt={`Gallery Image ${index + 1}`} />
+              </article>
+            })
+          }
+        </div>
+      </section>
+    </>
   )
 }
 
